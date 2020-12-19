83e26ee0f1ca46b528e4b0c3b6c4650d8b95a12a
Albert Suarez <alsumo95@gmail.com>
2020-12-19 17:40:35 +0100

    :sparkles: Implemented clinical cases endpoint

--- api/src/api/clinical_cases.py
+++ api/src/api/clinical_cases.py
@@ -1,6 +1,16 @@
+from src.engine.db_initializator import cases_db
 from src.helper import response_builder
 
 
 def get(case_id=None):
-    response = dict(results=[dict(case_id=1, sections=dict(medical_history='Lorem ipsum'))])
-    return response_builder.make(False, response=response)
+    if case_id:
+        if case_id in cases_db:
+            return response_builder.make(
+                False, response=dict(results=[dict(case_id=case_id, sections=cases_db.get(case_id))])
+            )
+        else:
+            return response_builder.make(True, message=f'Clinical case with id [{case_id}] could not be found.')
+    else:
+        return response_builder.make(
+            False, response=dict(results=[dict(case_id=k, sections=v) for k, v in cases_db.items()])
+        )
--- api/src/engine/db_initializator.py
+++ api/src/engine/db_initializator.py
@@ -4,7 +4,7 @@ from src import DATA_CLINICAL_CASES_DB, SECTION_LIST
 from src.helper import log
 
 
-def init():
+def _init():
     log.info('Initializing clinical cases database.')
     with open(DATA_CLINICAL_CASES_DB, 'rb') as f:
         db_dict = pickle.load(f)
@@ -13,3 +13,6 @@ def init():
             db_dict[key][section_name] = db_dict[key].get(section_name, None)
     log.info(f'DB initialized with {len(db_dict)} clinical cases.')
     return db_dict
+
+
+cases_db = _init()
