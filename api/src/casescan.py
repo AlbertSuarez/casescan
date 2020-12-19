b67ef18dd931e115b48eb8ac651f08c7b2a2fc26
Albert Suarez <alsumo95@gmail.com>
2020-12-19 17:30:17 +0100

    :card_file_box: Added initializer for DB

--- api/src/casescan.py
+++ api/src/casescan.py
@@ -2,12 +2,18 @@ import connexion
 
 from flask_cors import CORS
 
+from src.engine import db_initializator
 
+
+# API
 connexion_app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 flask_app = connexion_app.app
 flask_app.config['JSON_AS_ASCII'] = False
 connexion_app.add_api('openapi.yaml', arguments={'title': 'Casescan API'})
 CORS(flask_app)
+
+# Database
+clinical_db = db_initializator.init()
 
 
 @flask_app.route('/')
