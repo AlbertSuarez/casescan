b697ae109223402f1f20f933423d7570934f4730
Albert Suarez <alsumo95@gmail.com>
2020-12-20 00:22:39 +0100

    :sparkles: Implemented script for encoding text embeddings

---
+++ api/scripts/encode_embeddings.py
@@ -0,0 +1,86 @@
+import argparse
+import pickle
+import h5py
+import numpy as np
+import os
+
+from tqdm import tqdm
+
+from src import DATA_EMBEDDINGS, DATA_MAPPING, SECTION_LIST, SECTION_AGGREGATED, MODEL_DIMENSIONS
+from src.engine import transformer
+from src.engine.db_initializator import cases_db
+from src.helper import log
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_embeddings_file', type=str, default=DATA_EMBEDDINGS)
+    parser.add_argument('--output_mapping_file', type=str, default=DATA_MAPPING)
+    return parser.parse_args()
+
+
+def _validate(output_embeddings_file, output_mapping_file):
+    assert os.path.isdir(os.path.dirname(output_embeddings_file))
+    assert os.path.isdir(os.path.dirname(output_mapping_file))
+    log.info('Parameters validated!')
+
+
+def _encode(output_embeddings_file, output_mapping_file):
+    cases_items = cases_db.items()
+    mapping_dict = {k: dict() for k in SECTION_LIST + [SECTION_AGGREGATED]}  # Initialize dictionary mapping
+    with h5py.File(output_embeddings_file, 'w') as f:
+        # Create datasets
+        for dataset_name in mapping_dict.keys():
+            # Compute dataset shape
+            if dataset_name == SECTION_AGGREGATED:
+                dataset_shape = (len(cases_items), MODEL_DIMENSIONS * len(SECTION_LIST))
+            else:
+                dataset_shape = (len([x for _, x in cases_items if x.get(dataset_name)]), MODEL_DIMENSIONS)
+            # Create dataset
+            f.create_dataset(dataset_name, dataset_shape, dtype='f', maxshape=(None, None), chunks=True)
+
+        # Iterate over cases
+        for case_id, case_sections in tqdm(cases_items, total=len(cases_items)):
+            aggregated_embeddings = np.array([])
+            for section_name in SECTION_LIST:
+                section_str = case_sections.get(section_name)
+                # Encode embeddings if section is found
+                if section_str:
+                    section_embeddings = transformer.encode(section_str)
+                    aggregated_embeddings = np.concatenate((aggregated_embeddings, section_embeddings))
+                    section_embeddings_idx = len(mapping_dict[section_name])  # Compute current section index
+                    f[section_name][section_embeddings_idx] = section_embeddings
+                    mapping_dict[section_name][case_id] = section_embeddings_idx
+
+            # Save aggregated results
+            m = aggregated_embeddings.shape[0]
+            target_dims = f[SECTION_AGGREGATED].shape[1]
+            if m < target_dims:
+                new_aggregated = np.empty((target_dims, ))
+                new_aggregated[:m] = aggregated_embeddings
+                new_aggregated[m:] = aggregated_embeddings[np.random.choice(range(m), size=target_dims - m)]
+                aggregated_embeddings = new_aggregated
+
+            aggregated_embeddings_idx = len(mapping_dict[SECTION_AGGREGATED])  # Compute current aggregated index
+            f[SECTION_AGGREGATED][aggregated_embeddings_idx] = aggregated_embeddings
+            mapping_dict[SECTION_AGGREGATED][case_id] = aggregated_embeddings_idx
+
+    with open(output_mapping_file, 'wb') as f:
+        pickle.dump(mapping_dict, f)
+
+
+def _log(output_embeddings_file):
+    with h5py.File(output_embeddings_file, 'r') as f:
+        for dataset_name, dataset_object in f.items():
+            log.info(f'> Dataset {dataset_name}: [{dataset_object.shape}]')
+
+
+def main(output_embeddings_file, output_mapping_file):
+    _validate(output_embeddings_file, output_mapping_file)
+    _encode(output_embeddings_file, output_mapping_file)
+    _log(output_embeddings_file)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.output_embeddings_file, args.output_mapping_file)
--- api/src/__init__.py
+++ api/src/__init__.py
@@ -5,6 +5,8 @@ import os
 DATA_FOLDER = 'data'
 DATA_CLINICAL_CASES_FOLDER = os.path.join(DATA_FOLDER, 'covid-marato-clinical-cases')
 DATA_CLINICAL_CASES_DB = os.path.join(DATA_FOLDER, 'db.pkl')
+DATA_EMBEDDINGS = os.path.join(DATA_FOLDER, 'embeddings_full.h5')
+DATA_MAPPING = os.path.join(DATA_FOLDER, 'mapping.pkl')
 
 # Sections
 SECTION_MEDICAL_HISTORY = 'medical_history'
@@ -17,6 +19,7 @@ SECTION_LIST = [
     SECTION_MEDICAL_HISTORY, SECTION_PHYSIC_EXPLORATION, SECTION_SUPPLEMENTARY_TESTS,
     SECTION_ASSESSMENT, SECTION_TREATMENT, SECTION_EVOLUTION
 ]
+SECTION_AGGREGATED = 'aggregated'
 SECTION_MAPPING_FILE_TO_NAME = {
     'anamnesis': (SECTION_MEDICAL_HISTORY, False),
     'historia oncologica': (SECTION_MEDICAL_HISTORY, True),
@@ -46,3 +49,7 @@ SECTION_MAPPING_FILE_TO_NAME = {
     'evolucion y tratamiento': (SECTION_EVOLUTION, False)
 }
 SECTION_MAPPING_TO_SKIP = ['ver imagenes', 'ver a continuacion']
+
+# Transformer
+MODEL_NAME = 'average_word_embeddings_glove.840B.300d'
+MODEL_DIMENSIONS = 300
